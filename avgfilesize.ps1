f8391a4d29761c67b634fbdf0c6b1c93ba714427
Phydon <leann.phydon@gmail.com>
2025-03-24 20:02:35 +0100

    colourized output; skip errors

--- avgfilesize.ps1
+++ avgfilesize.ps1
@@ -7,31 +7,47 @@ $scriptName = $MyInvocation.MyCommand.Name
 
 # Check if the directory argument was provided
 if (-not $directory) {
-    Write-Output "Usage: .\$scriptName <PATH>"
+    Write-Host "Usage: ." -ForegroundColor Blue -NoNewline
+    Write-Host "\$scriptName" -ForegroundColor White -NoNewLine
+    Write-Host " <" -ForegroundColor Yellow -NoNewLine
+    Write-Host "PATH" -ForegroundColor Red -NoNewLine
+    Write-Host ">" -ForegroundColor Yellow 
     exit
 }
 
+# convert bytes into human-readable format
+function Convert-Size($size) {
+    if     ($size -ge 1GB) { return [math]::Round($size / 1GB, 2), "GB" }
+    elseif ($size -ge 1MB) { return [math]::Round($size / 1MB, 2), "MB" }
+    elseif ($size -ge 1KB) { return [math]::Round($size / 1KB, 2), "KB" }
+    else                   { return [math]::Round($size, 2), "B" }
+}
+
+# Initialize variables for total size and file count
+$totalSize = 0
+$fileCount = 0
+
 # Check if the directory exists
 if (Test-Path $directory) {
-    # Get the files recursively and calculate the average size
-    $stats = Get-ChildItem $directory -Recurse | Measure-Object -Property Length -Average
+    # Recursively process files in the directory
+    Get-ChildItem $directory -Recurse -File -ErrorAction SilentlyContinue | ForEach-Object {
+        # get the size of each file
+        $totalSize += $_.Length
+        $fileCount++
+    }
 
-    if ($stats.Count -gt 0) {
-        $avgSize = $stats.Average
+    # If we have files to process, calculate and output the average size
+    if ($fileCount -gt 0) {
+        $avgSize = $totalSize / $fileCount
+        $humanReadableSize, $unit = Convert-Size $avgSize
         
-        # convert bytes into human-readable format
-        function Convert-Size($size) {
-            if     ($size -ge 1GB) { "{0:N2} GB" -f ($size / 1GB) }
-            elseif ($size -ge 1MB) { "{0:N2} MB" -f ($size / 1MB) }
-            elseif ($size -ge 1KB) { "{0:N2} KB" -f ($size / 1KB) }
-            else                   { "{0} B" -f $size }
-        }
-        
-        # Output the average size in human-readable format
-        Write-Output "Average File Size: $(Convert-Size $avgSize)"
+        # Output the average file size with color formatting
+        Write-Host "Average File Size: " -NoNewline
+        Write-Host "$($humanReadableSize) " -ForegroundColor Red -NoNewline
+        Write-Host "$($unit)" -ForegroundColor Yellow
     } else {
-        Write-Output "No files found in the directory."
+        Write-Host "No accessible files found in the directory." -ForegroundColor White
     }
 } else {
-    Write-Output "The specified directory does not exist."
+    Write-Host "The specified directory does not exist." -ForegroundColor White
 }
