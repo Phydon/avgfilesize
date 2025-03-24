beca1283d2752c3df59b0043ff84d45a4e709165
Phydon <leann.phydon@gmail.com>
2025-03-24 21:12:56 +0100

    removed useless comments

--- avgfilesize.ps1
+++ avgfilesize.ps1
@@ -5,7 +5,6 @@ param (
 # Get the script file name dynamically
 $scriptName = $MyInvocation.MyCommand.Name
 
-# Check if the directory argument was provided
 if (-not $directory) {
     Write-Host "Usage: ." -ForegroundColor Blue -NoNewline
     Write-Host "\$scriptName" -ForegroundColor White -NoNewLine
@@ -23,25 +22,19 @@ function Convert-Size($size) {
     else                   { return [math]::Round($size, 2), "B" }
 }
 
-# Initialize variables for total size and file count
 $totalSize = 0
 $fileCount = 0
 
-# Check if the directory exists
 if (Test-Path $directory) {
-    # Recursively process files in the directory
     Get-ChildItem $directory -Recurse -File -ErrorAction SilentlyContinue | ForEach-Object {
-        # get the size of each file
         $totalSize += $_.Length
         $fileCount++
     }
 
-    # If we have files to process, calculate and output the average size
     if ($fileCount -gt 0) {
         $avgSize = $totalSize / $fileCount
         $humanReadableSize, $unit = Convert-Size $avgSize
         
-        # Output the average file size with color formatting
         Write-Host "Average File Size: " -NoNewline
         Write-Host "$($humanReadableSize) " -ForegroundColor Red -NoNewline
         Write-Host "$($unit)" -ForegroundColor Yellow
